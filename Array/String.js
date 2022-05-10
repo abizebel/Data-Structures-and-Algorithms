eff7679eacb6302eca2976013e8355282b14d8d4
Abbas Hosseini <abizebel@gmail.com>
2022-05-10 21:11:18 +0430

    Create String.js

---
+++ Array/String.js
@@ -0,0 +1,12 @@
+class String {
+  isPalindrome(str) {
+    let len = str.length;
+
+    for (i = 0; i < len / 2; i++) {
+      if (str[i] !== str[len - 1 - i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
