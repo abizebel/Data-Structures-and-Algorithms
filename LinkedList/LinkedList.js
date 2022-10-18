b314608f4bf1b6acd5c851e0aa0e60524cb3f256
Abbas Hosseini <abizebel@gmail.com>
2022-10-18 23:24:09 +0330

    Update LinkedList.js

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -1,68 +1,129 @@
 /*
     Singly linked list => it can be traversed in only one direction from head to the last node (tail)
-    head = first item
-    tail = last item
+    If we have just one node in liked list head=tail and they are same
     comparing object with the saem refrence is possible
     var x = {val:1} var y ={child:x}   y.child ===x
 */
 
 class LinkedList {
-  constructor(value) {
-    //head & tail is the first node when we have just one node
-    this.head = { value, next: null };
-    this.tail = this.head;
-  }
-
-  //inset new value to end of the linked list
-  insertAtEnd(val) {
-    const node = { value: val, next: null };
-    //new node is tail and head point to it
-    this.tail.next = node;
-    this.tail = node;
-  }
-
-  //remove a node
-  remove() {}
-
-  //remove the value at the end of the linked list
-  removeTail() {
-    //loop and find node before tail node.next === this.tail
-    let currentNode = this.head;
-    while (currentNode.next !== this.tail) {
-      currentNode = currentNode.next;
+    constructor() {
+        this.head = null; //First element of the list
+        this.tail = null; //Last element of the list
     }
 
-    //then new tail is node before previous tail and new not disconnected form it
-    currentNode.next = null;
-    this.tail = currentNode;
-  }
+    //inset new value to end of the linked list
+    append(val) {
+        const newNode = { value: val, next: null };
+        //newNode is stored in next property of the previously last node
+        //If we have just one node in liked list head=tail and they are same
+        if (this.tail) {
+            this.tail.next = newNode;
+        }
 
-  //search if value contains in linked list
-  contains(searchedValue) {
-    //loop untill node value is not equal to searchedValue
-    let currentNode = this.head;
-    while (currentNode.value !== searchedValue) {
-      currentNode = currentNode.next;
+        //[IMPORTANT DESCRIPTION] 
+        // there is a ambiguous code "this.tail = newNode" this not means we replace the old tail
+        // in memory, so the old tail still exist in memory becuase when run "this.head = newNode" other newNodes 
+        // continuesly liked together by refrence the Key is head for holding refrences
+        //we cant acces to old tail but this.head can access by refrence
+        this.tail = newNode;
+
+        //If we have just one node in liked list head=tail and they are same
+        //This just happen one time
+
+        if (!this.head) {
+            this.head = newNode;
+        }
+        //1- this.head={value : 1, next : null} / this.tail={value : 1, next : null}
+        //2- this.tail={value : 1, next : {value:2, next : null}} / this.tail={value : 2, next : null}
+
     }
 
-    return currentNode.value === searchedValues;
-  }
+    //inset new value to start of the linked list
+    prepend(val) {
+        const newNode = { value: val, next: this.head };
 
-  //check if a node is head of linked list
-  isHead(node) {
-    return node === this.head;
-  }
+        //this not means we replace the old head in memory, so the old tail still exist in memory
+        this.head = newNode;
 
-  //check if a node is tail of linked list
-  isTail(node) {
-    return node === this.tail;
-  }
+        //If we have just one node in liked list head=tail and they are same
+        if (!this.tail) {
+            this.tail = newNode
+        }
+    }
+
+    //convert linked list to array
+    toArray() {
+        const elements = [];
+        const currentNode = this.head;
+
+        while (currentNode) {
+            elements.push(currentNode)
+            currentNode = currentNode.next;
+        }
+
+        return elements;
+    }
+
+    //delete a node
+    delete(val) {
+
+        //it means liked list is empty
+        if (!this.head) {
+            return;
+        }
+
+        //if value found in head
+        while (this.head && this.head.value === val) {
+            this.head = this.head.next
+        }
+
+        //if value found after head
+        let currentNode = this.head;
+        while (currentNode.next) {
+            if (currentNode.next.value === val) {
+                currentNode.next = currentNode.next.next
+            } else {
+                currentNode = currentNode.next
+            }
+        }
+    }
+
+    //find a node
+    find(val) {
+        //it means liked list is empty
+        if (!this.head) {
+            return;
+        }
+
+        //if value found after head
+        let currentNode = this.head;
+        while (currentNode) {
+            if (currentNode.value === val) {
+                return currentNode
+            }
+            currentNode = currentNode.next
+        }
+
+        return null
+    }
+
+    insertAfter(val, afterValue) {
+
+        const existingValue = this.find(afterValue);
+        if (existingValue) {
+            const newNode = { value: val, next: existingValue };
+            //play play with refrences
+            existingValue.next = newNode
+        }
+    }
 }
 
-// const linkedList = new LinkedList(1);
-// linkedList.insertAtEnd(2);
-// linkedList.insertAtEnd(3);
-// const constainsThree = linkedList.contains(3)
-// console.log(constainsThree)
-// linkedList.removeTail()
-// console.log(linkedList)
+const linkedList = new LinkedList();
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+linkedList.prepend(0);
+linkedList.delete(0);
+linkedList.insertAfter(5, 2);
+console.log(linkedList)
+console.log(linkedList.find(1))
