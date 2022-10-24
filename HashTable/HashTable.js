cab32f1dd64d041e31d845d948f6b9a7993b32e0
Abbas Hosseini <abizebel@gmail.com>
2022-10-24 23:36:25 +0330

    Update HashTable.js

--- HashTable/HashTable.js
+++ HashTable/HashTable.js
@@ -1,54 +1,69 @@
-//Array Implementation
 class HashTable {
     constructor() {
-        this.storage = [];
-        this.tableSize = 24;
+        this.size = 16;
+        this.buckets = Array(16)
+            .fill(null)
+            //fot solving collisions we store multiple values per hash so assume any item as another array
+            .map(() => []);
     }
 
-    //insert a new key-value pair
-    insert(key, value) {
-        const index = this.hash(key, this.tableSize);
-        if (!this.storage[index]) {
-            this.storage[index] = [];
-        }
-
-        //TODO : check already key was inserted
-        this.storage[index].push([key, value]);
-    }
-
-    //insdeleteert a new key-value pair
-    remove() {}
-
-    //returns a value associated with a key
-    retrieve(key) {
-        const index = this.hash(key, this.tableSize);
-        const arrayAtIndex = this.storage[index];
-
-        if (arrayAtIndex) {
-            for (let i = 0; i < this.storage[index].length; i++) {
-                const keyValueArray = arrayAtIndex[i];
-                //[0,0,[['a',1]],0] => if key === 'a'
-                if (keyValueArray[0] === key) {
-                    return keyValueArray[1];
-                }
-            }
-        }
-    }
-
-    //hash strinbg into a integer can be merged to an array index
-    hash(str, size) {
-        let sum = 0;
-        for (let i = 0; i < str.length; i++) {
+    hash(key) {
+        let hash = 0;
+        for (const char of key) {
             //A simple way to create the hash would be to sum the ASCII code of the 
-            //in the key using the charCodeAt() method as follow
-            sum += str.charCodeAt(i) * 3;
-            //This means that any id we generate, will be a number from 0 to our specified size value (n)
-            return sum % size;
+            //in the key using the charCodeAt() method as  - example :Ab => 1 + 2 =3
+            hash += char.charCodeAt(0);
+        }
+        //This means that any we generate a number from 0 to our specified siz value(n)
+        return hash % this.size;
+    }
+
+    set(key, value) {
+        const keyHash = this.hash(key);
+        const bucketArray = this.buckets[keyHash];
+        const storedElement = bucketArray.find((element) => {
+            return element.key === key;
+        });
+
+        //check hash already has a value by checking bucketArray
+        // if the nested array already has a value we add the new value there after
+        // or if it has no value yet we add the new value as the first value in that nested array
+        if (storedElement) {
+            storedElement.val = value;
+        } else {
+            bucketArray.push({ key: key, val: value });
+        }
+    }
+
+    get(key) {
+        const keyHash = this.hash(key);
+        const bucketArray = this.buckets[keyHash];
+        const storedElement = bucketArray.find(element => {
+            return element.key === key;
+        });
+        return storedElement;
+    }
+
+    showInfo() {
+        for (const key in this.buckets) {
+            if (this.buckets[key] !== null) {
+                console.log(key, this.buckets[key]);
+            }
         }
     }
 }
 
-const hashTable = new HashTable();
-hashTable.insert("a", 1);
-hashTable.insert("b", 2);
-hashTable.retrieve("b");
+const table1 = new HashTable();
+for (const char of 'academind') {
+    table1.set(char, char);
+}
+
+for (const char of 'hello') {
+    table1.set(char, char);
+}
+
+for (const char of 'does this work') {
+    table1.set(char, char);
+}
+
+console.log(table1.showInfo());
