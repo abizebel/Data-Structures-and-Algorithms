b03b7dd1ff0e5e8a09077d26646b402453515006
Abbas Hosseini <abizebel@gmail.com>
2022-12-30 21:50:00 +0330

    Create IsPoerOfTwo-Bitwise.js

---
+++ _Algorithms_/isPowerOfTwo/IsPoerOfTwo-Bitwise.js
@@ -0,0 +1,16 @@
+// 2 => 0010
+// 4 => 0100
+// 4 => 1000
+function isPowerOfTwo(number) {
+    if (number < 1) {
+        return false;
+    }
+
+    return (number & (number - 1)) === 0; // O(1)
+}
+
+console.log(isPowerOfTwo(8));
+console.log(isPowerOfTwo(5));
+console.log(isPowerOfTwo(20));
+console.log(isPowerOfTwo(16));
+console.log(isPowerOfTwo(13));
